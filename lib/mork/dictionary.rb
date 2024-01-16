cff7c1d82b7cd4f1242a45a9f7fbc74986e2dcaa
Joe Yates <joe.g.yates@gmail.com>
2024-01-16 14:06:59 +0100

    Implement dictionary

---
+++ lib/mork/dictionary.rb
@@ -0,0 +1,32 @@
+require "mork/alias"
+require "mork/meta_alias"
+
+module Mork
+  class Dictionary
+    attr_reader :content
+
+    def initialize(content:)
+      @content = content
+    end
+
+    def to_h
+      aliases.each.with_object({}) { |a, acc| acc[a.key] = a.value }
+    end
+
+    # a - "Atom" - the data
+    # c - "Column" - the column name
+    def scope
+      meta&.scope || "a"
+    end
+
+    private
+
+    def aliases
+      @aliases ||= content.filter { |c| c.is_a?(Mork::Alias) }
+    end
+
+    def meta
+      @meta ||= content.find { |c| c.is_a?(Mork::MetaAlias) }
+    end
+  end
+end
---
+++ spec/mork/dictionary_spec.rb
@@ -0,0 +1,31 @@
+require "mork/dictionary"
+
+module Mork
+  RSpec.describe Dictionary do
+    subject { described_class.new(content: content) }
+
+    let(:content) do
+      [instance_double(Alias, key: "A", value: "a")]
+    end
+
+    describe "#to_h" do
+      it "returns a Hash of aliases" do
+        expect(subject.to_h).to eq({"A" => "a"})
+      end
+    end
+
+    describe "#scope" do
+      it "returns 'a'" do
+        expect(subject.scope).to eq("a")
+      end
+
+      context "when the dictionary has a meta alias" do
+        let(:content) { [instance_double(MetaAlias, scope: "foo")] }
+
+        it "returns the meta alias's scope" do
+          expect(subject.scope).to eq("foo")
+        end
+      end
+    end
+  end
+end
