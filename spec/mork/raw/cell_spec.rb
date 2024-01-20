ff5e0bc49f659a89a8828a658d5aaf1df87d19d6
Joe Yates <joe.g.yates@gmail.com>
2024-01-20 10:07:33 +0100

    Return a Resolved::Cell instance

--- spec/mork/raw/cell_spec.rb
+++ spec/mork/raw/cell_spec.rb
@@ -21,15 +21,23 @@ module Mork
       let(:dictionary_a) { {"Y" => "99"} }
       let(:result) { subject.resolve(dictionaries: dictionaries) }
 
-      it "returns a pair" do
-        expect(result).to eq(%w[ciao 42])
+      it "returns a Resolved::Cell" do
+        expect(result).to be_a(Resolved::Cell)
+      end
+
+      it "returns the key" do
+        expect(result.key).to eq("ciao")
+      end
+
+      it "returns the value" do
+        expect(result.value).to eq("42")
       end
 
       context "when the value is a reference" do
         let(:raw) { "^AA^Y" }
 
         it "resolves the reference" do
-          expect(result).to eq(%w[ciao 99])
+          expect(result.value).to eq("99")
         end
       end
     end
